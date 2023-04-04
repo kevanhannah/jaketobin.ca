c7e4f7c9601a91b3a4165801025325bfa85c8a1e
Kevan Hannah <25513354+kevanhannah@users.noreply.github.com>
2023-04-04 12:08:52 -0400

    unify buttons

--- sveltekit/src/components/Shop/ProductPage/AddToCartButton.svelte
+++ sveltekit/src/components/Shop/ProductPage/AddToCartButton.svelte
@@ -1,6 +1,6 @@
 <script>
-	import { addItemToCart, cart, isLoading } from '$lib/stores/storeContext.js';
-	$: console.log($cart);
+	import { addItemToCart, isLoading } from '$lib/stores/storeContext.js';
+  import Button from '$components/shared/Button.svelte';
 
 	export let variant;
 
@@ -8,44 +8,11 @@
 </script>
 
 {#if availableForSale}
-	<button
-		class:adding={$isLoading}
-		disabled={$isLoading}
+	<Button
+		loading={$isLoading}
 		on:click={addItemToCart({ variantId: id })}
-		>{$isLoading ? 'Processing...' : 'Add to cart'}</button
+		>{$isLoading ? 'Processing...' : 'Add to cart'}</Button
 	>
 {:else}
-	<button class="disabled" disabled>Out of stock</button>
+  <Button disabled>Out of stock</Button>
 {/if}
-
-<style lang="postcss">
-	button {
-		padding: 0.75em 1.25em;
-		color: var(--paperWhite);
-		background: var(--black);
-		border: 1px solid var(--black);
-		border-radius: 0.5em;
-		font-weight: 600;
-		transition-property: border-color, background, color, transform, box-shadow;
-		transition-duration: 0.15s;
-		transition-timing-function: ease;
-	}
-
-	button:not(.adding):hover {
-		color: var(--black);
-		background: var(--paperWhite);
-	}
-
-	.adding {
-		background: var(--mediumGray);
-		border: 1px solid var(--mediumGray);
-		cursor: default;
-	}
-
-	.disabled {
-		background: var(--paperWhite);
-		color: var(--black);
-		border: 1px solid var(--black);
-		cursor: default;
-	}
-</style>
--- sveltekit/src/components/shared/Button.svelte
+++ sveltekit/src/components/shared/Button.svelte
@@ -1,14 +1,40 @@
 <script>
+  export let disabled = false;
+  export let loading = false;
 	export let style = undefined;
 	export let type = 'button';
 </script>
 
-<button {style} {type}>
+<button {disabled} {loading} on:click {style} {type}>
 	<slot />
 </button>
 
-<style>
-	button {
+<style lang="postcss">
+  button {
+		padding: 0.75em 1.25em;
+		color: var(--paperWhite);
+		background: var(--black);
+		border: 1px solid var(--black);
+		border-radius: 0.5em;
+		font-weight: 600;
+		transition-property: border-color, background, color, transform, box-shadow;
+		transition-duration: 0.15s;
+		transition-timing-function: ease;
+	}
+
+  button:disabled {
+		background: var(--paperWhite);
+		color: var(--black);
+		border: 1px solid var(--black);
+		cursor: default;
+	}
+
+  button:not(:disabled)[loading="true"] {
+    background: var(--mediumGray);
+		border: 1px solid var(--mediumGray);
+		cursor: default;
+  }
+	/* button {
 		padding-top: 1em;
 		padding-bottom: 1em;
 		padding-left: 1.25em;
@@ -16,12 +42,17 @@
 		background-color: var(--black);
 		border-radius: 0.5em;
 		color: var(--paperWhite);
-	}
+	} */
 
 	@media (hover: hover) {
-		button:hover {
+    button:not(:disabled)[loading="false"]:hover {
+		color: var(--black);
+		background: var(--paperWhite);
+    cursor: pointer;
+	}
+		/* button:hover {
 			background-color: #6c6c6c;
-		}
+		} */
 	}
 
 	@media (max-width: 768px) {
