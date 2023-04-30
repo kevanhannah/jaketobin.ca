a71834e8d7c02dd982275f904cb1c1716c94824c
Kevan Hannah <25513354+kevanhannah@users.noreply.github.com>
2023-04-30 19:46:41 -0400

    better error handling

--- sveltekit/src/routes/shop/+page.server.js
+++ sveltekit/src/routes/shop/+page.server.js
@@ -4,22 +4,26 @@ import singletonQuery from '$lib/utils/singletonQuery';
 
 export async function load() {
 	try {
-		const { pageContent } = await client.fetch(singletonQuery('shop'));
+		const res = await client.fetch(singletonQuery('shop'));
 
-		if (pageContent?.body) {
-			return {
-				pageContent: {
-					...pageContent,
-					seo: {
-						title: pageContent.seo?.title || 'Shop',
-						...pageContent.seo,
-					},
-				},
-			};
+		if (!res) {
+			throw new Error('Failed to load page data');
 		}
-	} catch (err) {
+
+		const { pageContent } = res;
+
+		return {
+			pageContent: {
+				...pageContent,
+				seo: {
+					title: pageContent.seo?.title || 'Shop',
+					...pageContent.seo,
+				},
+			},
+		};
+	} catch ({ message }) {
 		throw error(404, {
-			message: 'Something went wrong',
+			message,
 		});
 	}
 }
